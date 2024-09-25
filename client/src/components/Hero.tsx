accc64fee479f4071ca764b89e716627884561c7
Abdulrehman <abdul.tariq6756@gmail.com>
2024-09-25 22:04:35 +0500

    fix:Hero section responsiveness

--- client/src/components/Hero.tsx
+++ client/src/components/Hero.tsx
@@ -2,15 +2,16 @@
 export const Hero = () => {
   return (
     <section className="h-full w-full flex flex-col md:flex-row items-center justify-center overflow-hidden">
-      <div className="w-full h-full flex flex-col justify-center items-start pl-6 pr-6 mt-28">
+      <div className="w-full h-full md:w-1/2 flex flex-col justify-center items-start pl-6 md:pl-20 md:pb-32 mt-28">
       <h1 className="text-4xl text-left font-extrabold md:text-5xl">Find, book, rent a car-quick and super easy!</h1>
       <p className="text-left mt-6 font-thin md:text-xl">Streamline your car rental experience with our effortless booking process.</p>
       <button className="w-[110px] h-[35px] ring-1 rounded-full bg-primary-blue mt-6 text-white text-sm">Explore cars</button>
       </div>
-      <div className="w-full h-full flex flex-row md:flex-col justify-end items-end md:overflow-hidden relative">
-        <img src='/hero.png' className="w-[450px] sm:w-[550px] md:w-[650px]" />
+      <div className="w-full h-full md:w-1/2 flex items-start justify-end flex-row md:flex-col md:overflow-hidden relative">
+        <img src='/hero.png' className="w-[450px] sm:w-[550px] md:min-w-[50vw]" ></img>
         <div className="bg-hero-bg bg-repeat-round h-[300px] w-[450px] md:w-[1000px] md:h-[600px] absolute -z-30 -right-20 -top-8 md:-top-14 md:left-10"></div>
       </div>
     </section>
   )
 }
+``
--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -3,9 +3,9 @@ export const Navbar = () => {
   return (
     <div className='h-14 w-full absolute top-0'>
         <nav className="h-full w-full flex flex-row justify-center items-center">
-            <ul className="h-full w-full flex flex-row items-center justify-between pl-6 pr-10">
+            <ul className="h-full w-full flex flex-row items-center justify-between pl-6 pr-10 md:p-[75px]">
                 <li>
-                    <img className="w-28" src='/logo.svg' />
+                    <img className="w-24" src='/logo.svg' />
                 </li>
                 <li>
                     <button className="ring-1 ring-primary-blue rounded-full bg-[#f2f2f2] w-24 h-8 flex items-center justify-center text-primary-blue">Sign in</button>
